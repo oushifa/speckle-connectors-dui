983b3b56f45c647dc0e08ba9bb80009917280f96
ukinll <ukinll@163.com>
2026-04-20 10:58:12 +0800

    feat:192

--- env-config.ts
+++ env-config.ts
@@ -1,5 +1,6 @@
 const config: Record<string, string> = {
-  serverUrl: 'http://47.100.77.97:64482'
+  // serverUrl: 'http://47.100.77.97:64482'
+  serverUrl: 'http://192.168.154.28'
 }
 
 const getConfig = (key: string) => {
